efdc53bdc83b01ccc39fbcd57cc40cd29a394327
codex1190 <joshmodrigo01@gmail.com>
2025-11-10 14:08:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const { createClient } = require('bedrock-protocol');
 const express = require('express');
-const fs = require('fs');
-
-// Load behavior functions
-const { handleNightSafety, handleHunger, handleMobAvoidance, handlePathing, respawnIfDead } = require('./behaviors');
 const behaviorManager = require('./behaviors/behaviorManager');
 
-// Create a basic Express server to keep the bot alive
+// Express server to keep alive
 const app = express();
 app.get('/', (_, res) => res.send('Bot is alive!'));
 app.listen(3000, () => console.log('Keep-alive server running on port 3000'));
@@ -20,37 +16,12 @@ const bot = createClient({
   version: '1.21.120'
 });
 
-let isNight = false;
-
-// Bot event handlers
+// Start behaviors when spawned
 bot.on('spawn', () => {
-  console.log('Noxell joined the server!');
-  console.log('Noxell spawned! Starting behaviors...');
-
-  walkLoop(bot);
-  handlePathing(bot);
+  console.log('Bot spawned! Starting AI behaviors...');
+  behaviorManager(bot); // handles walking, pathing, safety, etc.
 });
 
-bot.on('time', (packet) => {
-  const time = packet.time;
-  isNight = time > 13000;
-});
-
-bot.on('update_attributes', () => {
-  handleHunger(bot);
-});
-
-bot.on('mob_spawn', () => {
-  handleMobAvoidance(bot);
-});
-
-bot.on('death_info', () => {
-  respawnIfDead(bot);
-});
-
-// Night safety check every 10 seconds
-setInterval(() => {
-  if (isNight) {
-    handleNightSafety(bot);
-  }
-}, 10000);
+bot.on('text', (packet) => console.log(`[Server] ${packet.message}`));
+bot.on('error', (err) => console.error('Bot error:', err));
+bot.on('kick', (packet) => console.log('Kicked from server:', packet.reason));
