e90875b9a787e974c8e5bad67eeee2870d0b2ced
mrswish19 <mrswish001@gmail.com>
2025-12-22 11:09:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ================= IMPORTS =================
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('metacoresrv');
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const net = require('net');
