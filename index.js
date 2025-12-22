c8aa48320dc63a6c08fd2674aa5c2891ef73c7b0
mrswish19 <mrswish001@gmail.com>
2025-12-22 12:02:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const RENDER_URL = process.env.RENDER_URL || 'https://metacoresrv.onrender.com';
 const SERVER = {
   host: 'metacoresrv.aternos.me',
   port: 36614,
-  username: 'TimeGuardBot',
+  username: 'Alisha2930',
   version: '1.21.120',
   offline: true
 };
